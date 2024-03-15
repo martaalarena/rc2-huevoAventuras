971dbd0d254e7fd7bfc5c4b6709c2aa8bff0c771
Marta <martalarena2002@gmail.com>
2024-03-15 11:31:59 +0100

    post ya da 200 pero mal

--- backend/src/models/PitaRentingModel.py
+++ backend/src/models/PitaRentingModel.py
@@ -1,16 +1,18 @@
 from database.db import get_connection
 
+from datetime import datetime
+
 class PitaRentingModel():
     @classmethod
-    def add_pita(self, pitaRenting):
+    def add_pitaRenting(cls, pitaRenting):
         try:
-            connection=get_connection()
+            connection = get_connection()
 
             with connection.cursor() as cursor:
-                    cursor.execute("""INSERT INTO pitas (id, pick_up_date, return_date, id_pita)
-                                                VALUES (%s, %s, %s, %s, %s,)""", (pitaRenting.id, pitaRenting.pick_up_date, pitaRenting.return_date, pitaRenting.id_pita))
-                    affected_rows=cursor.rowcount
-                    connection.commit()
+                cursor.execute("""INSERT INTO pitarentings (pick_up_date, return_date, id_pita)
+                                        VALUES (%s, %s, %s)""", (pitaRenting.pick_up_date, pitaRenting.return_date, pitaRenting.id_pita))
+                affected_rows = cursor.rowcount
+                connection.commit()
 
             connection.close()
             return affected_rows
--- backend/src/routes/PitaRenting.py
+++ backend/src/routes/PitaRenting.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 
+
 #Models
 from models.entities.PitaRenting import PitaRenting
 from models.PitaRentingModel import PitaRentingModel
@@ -7,16 +8,17 @@ from models.PitaRentingModel import PitaRentingModel
 main = Blueprint('pitarentings_blueprint', __name__)
 
 @main.route('/add', methods=['POST'])
-def add_pita():
+def add_pitaRenting():
     try:
         pick_up_date = request.json['pick_up_date']
         return_date = request.json['return_date']
-        pita = PitaRenting(str(id),pick_up_date,return_date)
 
-        affected_rows = PitaRentingModel.add_pita(pita)
+        pitaRenting = PitaRenting(pick_up_date,return_date)
+
+        affected_rows = PitaRentingModel.add_pitaRenting(pitaRenting)
 
         if affected_rows == 1:
-            return jsonify(pita.id)
+            return jsonify(pitaRenting.id)
         else:
             return jsonify({'message': "Error on insert"}), 500
 
